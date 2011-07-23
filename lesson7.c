7c235e3db4a420cde814c976ef9ed46610688daa
psjicfh <psjicfh@yeah.net>
2011-07-23 17:22:30 +0800

    this project have three file
    this project have three file

--- lesson7.c
+++ lesson7.c
@@ -1,12 +1,7 @@
-#include <stdio.h>
-#define HOW_MANY_TIMES_TO_SAY_HELLO 10000
+#include "say_three_hi.h"
 int main(int argc, const char *argv[])
 {	
-	int i;
-	for (i = 0; i < HOW_MANY_TIMES_TO_SAY_HELLO; i++) 
-	{
-		printf("hello world!\n");
-	}
-	return 0;
-}  //宏定义用法 
-//git commit -a 里面的vim文件不能写汉字
+    say_three_hi();
+    return 0;
+} 
+
